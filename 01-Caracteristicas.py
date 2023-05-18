bab6819ae42b4e783bdd7b31ef72697ca4aaacae
Luem <luchemma@gmail.com>
2023-05-18 18:56:20 -0300

    caracteristicas de python resumen

---
+++ 01-Caracteristicas.py
@@ -0,0 +1,18 @@
+# Caracteristicas de Python:
+
+# Es un lenguaje de tipado dinamico, es decir, la variable se adapta segun el tipo de dato que ingresemos.
+# Es orientado a objetos.
+# Proposito general.
+# De alto nivel
+
+# Lenguaje interpretado, es decir, hay un programa que interpreta instruccion tras instruccion, linea por linea.
+# - PRO: Facil de debuggear.
+# - CONTRA: Menos eficiente que los lenguajes que compilan.
+
+# Contiene gran cantidad de librerias, y una gran comunidad de desarrolladores.
+# Multiparadigma, sirve para programar en diferentes plataformas. (Windows, Linux, Mac)
+
+# Es uno de los mejores pagos del mercado.
+
+# Se utiliza mucho para desarrollo web, para aplicaciones moviles y desktop. Automatizacion con scripts.
+# Realizar tareas de Ciencia de Datos y Machine learning (rama de inteligencia artificial), para el analisis de datos. Desarrollo de software en general. Automatizacion de pruebas de software (QA). Se puede tambien desarrollar juegos.
