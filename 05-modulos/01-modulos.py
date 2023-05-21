c4ccd1535eb570ee4bc6018af4b73875b5f02102
Luem <luchemma@gmail.com>
2023-05-20 23:49:01 -0300

    modulos

---
+++ 05-modulos/01-modulos.py
@@ -0,0 +1,25 @@
+# Dato interesante: Los modulos integrados de Python estan escritos en C
+# Los nombres de los archivos no deben contener guiones medios.
+
+# Es un modulo (namespace)
+import modulo_saludar
+
+# Podemos importarlo y asignarle un nickname
+import modulo_saludar as ms
+
+# Importar algunas funciones del modulo
+from modulo_saludar import saludoEnChino, saludoEnPortugues
+
+print(modulo_saludar.saludar("Jorgito"))
+print(ms.saludar("Roberto"))
+print(saludoEnChino("Pepe"))
+print(saludoEnPortugues("Juan"))
+
+# Tipo de dato de modulo_saludar
+print(type(modulo_saludar))
+
+# Para ver las propiedades y metodos de un modulo
+print(dir(modulo_saludar))
+
+# Accedemos al nombre original del modulo.
+print(ms.__name__)
---
+++ 05-modulos/modulo_saludar.py
@@ -0,0 +1,10 @@
+def saludar(name: str):
+    return f"Hola {name}, espero que estes muy bien"
+
+
+def saludoEnChino(name: str):
+    return f"你好{name}，我希望你一切顺利"
+
+
+def saludoEnPortugues(name: str):
+    return f"Olá {name}, espero que este bem"
